f5342f8e8af67c9648f47df522a2b7e2ac0e0a65
saad <saadd@student.iul.ac.in>
2023-10-10 19:43:07 +0530

    reg and login page added

--- app/collections/components/register/register.jsx
+++ app/collections/components/register/register.jsx
@@ -22,9 +22,15 @@ const Register = ({ onClose }) => {
             height={200}
             src="/logo-light.png"
           />
-          <h1 className="text-black dark:text-white text-2xl py-5">
-            Log in to continue
-          </h1>
+          <h1 className="text-black dark:text-white text-2xl py-5">Register</h1>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Name"
+              className="text-white bg-transparent bg-opacity-20 rounded-md w-full pl-10 pr-4 py-2 border-b border-black dark:border-white focus:outline-none focus:border-gray-400"
+            />
+            <i className="fa  fa-user absolute left-3 top-1/2 transform -translate-y-1/2 text-black dark:text-white"></i>
+          </div>
           <div className="relative">
             <input
               type="text"
@@ -35,14 +41,25 @@ const Register = ({ onClose }) => {
           </div>
           <div className="relative">
             <input
-              type="password"
-              placeholder="Password"
+              type="text"
+              placeholder="Category"
+              className="text-white bg-transparent bg-opacity-20 rounded-md w-full pl-10 pr-4 py-2 border-b border-black dark:border-white focus:outline-none focus:border-gray-400"
+            />
+            <i className="fa fa-list-alt absolute left-3 top-1/2 transform -translate-y-1/2 text-black dark:text-white"></i>
+          </div>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Message"
               className="mb-5 text-white bg-transparent bg-opacity-20 rounded-md w-full pl-10 pr-4 py-2 border-b border-black dark:border-white focus:outline-none focus:border-gray-400"
             />
-            <i className="fa fa-lock absolute left-3 top-1/2 transform -translate-y-1/2 text-black dark:text-white"></i>
+            <i className="pb-5 fa  fa-comment absolute left-3 top-1/2 transform -translate-y-1/2 text-black dark:text-white"></i>
           </div>
-          <button className=" px-5   bg-white  hover:bg-gradient-to-r hover:opacity-50 px-4 py-2 rounded-lg transition duration-300 ease-in-out">
-            Login
+          <h1 className="text-white text-center px-11">
+            You will receive an eamil containing your password to login
+          </h1>
+          <button className=" my-5 bg-white  hover:bg-gradient-to-r hover:opacity-50 px-10 py-2 rounded-lg transition duration-300 ease-in-out">
+            Register
           </button>
         </div>
       </div>
